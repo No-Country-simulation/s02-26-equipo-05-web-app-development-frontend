1169f19ba193c2bcbf41677cdc93a1c9f003bda8
Catriel42 <catopereira72@gmail.com>
2026-02-25 17:31:18 -0400

    feat: Adding lazy routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pages/pricing/pricing').then((m) => m.Pricing),
+  },
+  {
+    path: 'services',
+    loadComponent: () => import('./pages/services/services').then((m) => m.Services),
+  },
+  {
+    path: 'about-us',
+    loadComponent: () => import('./pages/about-us/about-us').then((m) => m.AboutUs),
+  },
+  {
+    path: 'onboarding',
+    loadComponent: () => import('./pages/onboarding/onboarding').then((m) => m.Onboarding),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
